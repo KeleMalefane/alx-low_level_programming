71e337a48e9790c66bba7fd339be2d7368a12a28
KeleMalefane <kelebohilemalefane@gmail.com>
2023-10-11 21:03:55 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function _isapha -checks for lphabetic character 
+ * us ASCII table as your guide
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122 ))
+{return (1);
+}
+return  (0);
+}
